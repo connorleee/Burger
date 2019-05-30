f33b48e79b7aaca1439108e00998bcdab069e1e5
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-05-29 21:20:55 -0700

    worked on ORM. still need to finish updateOne

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,44 @@
-const connection = require("connection");
+const connection = require("./connection");
 
+function printQuestionMarks(num) {
+    var arr = [];
 
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+const orm = {
+    selectAll: function (table, cb) {
+        let queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function (table, colToSet, condition, cb) {
+        let queryString = "UPDATE " + table;
+
+        queryString
+    }
+}
 
 module.exports = orm;
