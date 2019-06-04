4403184dc170e79011ddecb32e75d4af548a672a
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-06-03 23:47:30 -0700

    front end JS is linked with html. jquery click handler is struggling right now for some reason.

---
+++ public/assets/js/burgerFrontEnd.js
@@ -0,0 +1,11 @@
+console.log("test load")
+
+$("#burgerInput").click(() => {
+    // event.preventDefault();
+
+    console.log("test")
+
+    var burgerName = $("#burgerName").val();
+
+    console.log(burgerName)
+})
