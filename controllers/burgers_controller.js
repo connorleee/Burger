b5bc111a915a6579a5b95426e21438abc48220ab
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-06-05 21:12:00 -0700

    got devour button to work. issue derived from arrow function which was not creating context, so this keyword was picking up the global window.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ router = express.Router();
 
 router.get("/", function (req, res) {
     burger.all(function (data) {
-        console.log(data)
+        // console.log(data)
 
         let hbsObject = {
             burgers: data,
@@ -19,7 +19,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    console.log(req.body)
+    // console.log(req.body)
 
     burger.create(
         "burger_name", req.body.burgerName, function (result) {
@@ -27,5 +27,22 @@ router.post("/api/burgers", function (req, res) {
         }
     );
 });
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
 
 module.exports = router;
--- public/assets/js/burgerFrontEnd.js
+++ public/assets/js/burgerFrontEnd.js
@@ -1,10 +1,8 @@
-console.log("test load")
-
-$(function () {
+$(document).ready(function() {
     $("#burgerInput").click(function (event) {
         event.preventDefault();
 
-        let newBurger = {burgerName: $("#burgerName").val().trim()}
+        let newBurger = { burgerName: $("#burgerName").val().trim() }
 
         console.log(newBurger)
 
@@ -12,8 +10,29 @@ $(function () {
             type: "POST",
             data: newBurger,
             dataType: "text"
-        }).then(()=>{
+        }).then(() => {
             console.log("created new burger");
+            location.reload();
+        })
+    })
+
+    $(".devour").on("click", function() {
+        console.log("devour button clicked")
+
+        console.log($(this))
+
+        let id = $(this).data("id")
+        let newDevouredState = {
+            devoured: true
+        };
+
+        console.log(id)
+
+        $.ajax("api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(() => {
+            console.log("changed devoured state to: " + newDevouredState);
             location.reload();
         })
     })
